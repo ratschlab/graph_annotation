509bc1b8b8bae84023806da32a6bff9d808475a0
Harun Mustafa <harun.mustafa@inf.ethz.ch>
2018-05-30 13:41:13 +0200

    Messages after unit tests fail

--- wavelet_trie/tests/test_wavelet_trie.cpp
+++ wavelet_trie/tests/test_wavelet_trie.cpp
@@ -1,76 +1,6 @@
 #include "gtest/gtest.h"
 #include "wavelet_trie.hpp"
 
-std::vector<annotate::cpp_int> generate_nums(std::vector<std::set<size_t>> &bits) {
-    std::vector<annotate::cpp_int> nums;
-    for (auto &num : bits) {
-        nums.emplace_back(0);
-        for (auto &i : num) {
-            annotate::bit_set(nums.back(), i);
-        }
-    }
-    return nums;
-}
-
-void check_wtr(annotate::WaveletTrie &wt, const std::vector<annotate::cpp_int> &nums) {
-    ASSERT_EQ(nums.size(), wt.size());
-    for (size_t i = 0; i < nums.size(); ++i) {
-        EXPECT_EQ(nums.at(i), wt.at(i));
-    }
-}
-
-// Note: the vector needs to be a copy, since WaveletTrie modifies it
-void generate_wtr(std::vector<annotate::WaveletTrie*> &wtrs, std::vector<annotate::cpp_int> nums, size_t step = -1llu) {
-    auto it = nums.begin();
-    while (step < -1llu && it + step <= nums.end()) {
-        wtrs.push_back(new annotate::WaveletTrie(it, it + step));
-        it += step;
-    }
-    if (it != nums.end())
-        wtrs.push_back(new annotate::WaveletTrie(it, nums.end()));
-}
-
-// TODO: this only works as a pointer, since destructor for int_vector fails when static
-annotate::WaveletTrie* merge_wtrs(std::vector<annotate::WaveletTrie*> &wtrs) {
-    annotate::WaveletTrie *wts = new annotate::WaveletTrie();
-    for (auto &wtr : wtrs) {
-        wts->insert(*wtr);
-        delete wtr;
-    }
-    return wts;
-}
-
-void test_wtr(std::vector<annotate::cpp_int>&& nums) {
-    constexpr size_t step = 2;
-    std::vector<annotate::WaveletTrie*> wtrs;
-
-    generate_wtr(wtrs, nums, step);
-    auto wts = merge_wtrs(wtrs);
-    check_wtr(*wts, nums);
-    delete wts;
-    wtrs.clear();
-
-    generate_wtr(wtrs, nums);
-    wts = merge_wtrs(wtrs);
-    check_wtr(*wts, nums);
-    delete wts;
-}
-
-void test_wtr_pairs(std::vector<annotate::cpp_int>&& nums1, std::vector<annotate::cpp_int>&& nums2) {
-    constexpr size_t step = 2;
-    std::vector<annotate::WaveletTrie*> wtrs;
-    std::vector<annotate::cpp_int> ref;
-    ref.reserve(nums1.size() + nums2.size());
-    ref.insert(ref.end(), nums1.begin(), nums1.end());
-    ref.insert(ref.end(), nums2.begin(), nums2.end());
-
-    generate_wtr(wtrs, nums1, step);
-    generate_wtr(wtrs, nums2, step);
-    auto wts = merge_wtrs(wtrs);
-    check_wtr(*wts, ref);
-    delete wts;
-    wtrs.clear();
-}
 
 std::vector<std::vector<std::set<size_t>>> bits {
     {}, // 0
@@ -196,16 +126,93 @@ std::vector<std::vector<std::set<size_t>>> bits {
     }
 };
 
+
+std::vector<annotate::cpp_int> generate_nums(std::vector<std::set<size_t>> &bits) {
+    std::vector<annotate::cpp_int> nums;
+    for (auto &num : bits) {
+        nums.emplace_back(0);
+        for (auto &i : num) {
+            annotate::bit_set(nums.back(), i);
+        }
+    }
+    return nums;
+}
+
+void check_wtr(annotate::WaveletTrie &wt,
+               const std::vector<annotate::cpp_int> &nums,
+               const std::string &message = std::string()) {
+    ASSERT_EQ(nums.size(), wt.size()) << message << ": size fail" << std::endl;
+    for (size_t i = 0; i < nums.size(); ++i) {
+        EXPECT_EQ(nums.at(i), wt.at(i)) << message << ":" << std::to_string(i) << std::endl;
+    }
+}
+
+// Note: the vector needs to be a copy, since WaveletTrie modifies it
+void generate_wtr(std::vector<annotate::WaveletTrie*> &wtrs, std::vector<annotate::cpp_int> nums, size_t step = -1llu) {
+    auto it = nums.begin();
+    while (step < -1llu && it + step <= nums.end()) {
+        wtrs.push_back(new annotate::WaveletTrie(it, it + step));
+        it += step;
+    }
+    if (it != nums.end())
+        wtrs.push_back(new annotate::WaveletTrie(it, nums.end()));
+}
+
+// TODO: this only works as a pointer, since destructor for int_vector fails when static
+annotate::WaveletTrie* merge_wtrs(std::vector<annotate::WaveletTrie*> &wtrs) {
+    annotate::WaveletTrie *wts = new annotate::WaveletTrie();
+    for (auto &wtr : wtrs) {
+        wts->insert(*wtr);
+        delete wtr;
+    }
+    return wts;
+}
+
+void test_wtr(size_t i) {
+    auto nums = generate_nums(bits[i]);
+    constexpr size_t step = 2;
+    std::vector<annotate::WaveletTrie*> wtrs;
+
+    generate_wtr(wtrs, nums, step);
+    auto wts = merge_wtrs(wtrs);
+    check_wtr(*wts, nums);
+    delete wts;
+    wtrs.clear();
+
+    generate_wtr(wtrs, nums);
+    wts = merge_wtrs(wtrs);
+    check_wtr(*wts, nums, std::to_string(i));
+    delete wts;
+}
+
+void test_wtr_pairs(size_t i, size_t j) {
+    auto nums1 = generate_nums(bits[i]);
+    auto nums2 = generate_nums(bits[j]);
+    constexpr size_t step = 2;
+    std::vector<annotate::WaveletTrie*> wtrs;
+    std::vector<annotate::cpp_int> ref;
+    ref.reserve(nums1.size() + nums2.size());
+    ref.insert(ref.end(), nums1.begin(), nums1.end());
+    ref.insert(ref.end(), nums2.begin(), nums2.end());
+
+    generate_wtr(wtrs, nums1, step);
+    generate_wtr(wtrs, nums2, step);
+    auto wts = merge_wtrs(wtrs);
+    check_wtr(*wts, ref, std::to_string(i) + "," + std::to_string(j));
+    delete wts;
+    wtrs.clear();
+}
+
 TEST(WaveletTrie, TestSingle) {
-    for (auto &bitset : bits) {
-        test_wtr(generate_nums(bitset));
+    for (size_t i = 0; i < bits.size(); ++i) {
+        test_wtr(i);
     }
 }
 
 TEST(WaveletTrie, TestPairs) {
-    for (auto &bitset1 : bits) {
-        for (auto &bitset2 : bits) {
-            test_wtr_pairs(generate_nums(bitset1), generate_nums(bitset2));
+    for (size_t i = 0; i < bits.size(); ++i) {
+        for (size_t j = 0; j < bits.size(); ++j) {
+            test_wtr_pairs(i, j);
         }
     }
 }
